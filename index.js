52aa0293e29a6c528daaadfbbc4a45ce64b704ed
mkoszutowski <mkoszutowski@divante.pl>
2020-04-07 12:08:01 +0200

    WIP - check by farmer

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const axios = require('axios');
 
 const delay = ms => new Promise(r => setTimeout(r, ms));
 const getDeployUrl = (version, namespace) => `https://${version}.${namespace}.preview.storefrontcloud.io`
-
+const getCheckUrl = (version, namespace) => `'https://farmer.storefrontcloud.io/deploy_check/${namespace}/${version}`
 
 ;(async function() {
 
     const githubToken = core.getInput('token');
     const namespace = core.getInput('namespace');
+    const username = core.getInput('username');
+    const password = core.getInput('password');
     const { sha: commitHash, repo, payload, issue} = github.context
 
     const prNumber = payload.pull_request && payload.pull_request.number
@@ -23,19 +25,28 @@ const getDeployUrl = (version, namespace) => `https://${version}.${namespace}.pr
     console.log(`Starting deploying PR #${prNumber} on ${deployUrl}`);
 
     let isSuccess = false;
+    const response = await axios.get(deployUrl);
     // try to get the success result for 8 times
     for (i = 0; i < 8; i++) {
-      const response = await axios.get(deployUrl);
       console.log(`.`);
       try {
-          if (response.data.includes('<html data-n-head-ssr')) {
-            console.log(`Your application is successfully deployed.`);
-            core.setOutput('preview_url', deployUrl);
-            isSuccess = true
-            break;
-          } else {
-            console.log(`.`);
-          }
+        var checkUrl = getCheckUrl(commitHash, namespace)
+        var checkResponse = await axios.get(checkUrl);
+        try {
+          var data = JSON.parse(checkResponse.data)
+        } catch (e) {
+          console.log(`.`);
+          break;
+        }
+
+        if (data.deployed == '1' && data.ready == '1') {
+          console.log(`Your application is successfully deployed.`);
+          core.setOutput('preview_url', deployUrl);
+          isSuccess = true
+          break;
+        } else {
+          console.log(`.`);
+        }
       } catch (e) {
         console.log('e');
       }
